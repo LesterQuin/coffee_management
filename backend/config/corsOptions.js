e9ce4342a3eeb4c3455e526ca5a5c70c3eb988bc
Lester Quinones <lezhter@gmail.com>
2025-12-01 11:04:33 +0800

    update corsopts

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,9 +1,22 @@
 // backend/config/corsOptions.js
 import allowedOrigins from "./allowed-origins.js";
 
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       console.warn("❌ Blocked by CORS:", origin);
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin) || origin === "null") {
       callback(null, true);
     } else {
       console.warn("❌ Blocked by CORS:", origin);
@@ -13,5 +26,6 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
 };
+
 
 export default corsOptions;
